97ff10c98320cdd2cdf55d0393059614eeb518c9
Nehashaikh913 <nehashaikh0500@gmail.com>
2023-05-10 19:18:19 +0530

    fetched all product category blogs authors logout functionality

--- backend/controller/registerController.js
+++ backend/controller/registerController.js
@@ -2,15 +2,26 @@ const con = require("../config");
 
 const registerUser = (req, res) =>{
     const {username , password} = req.body;
-    con.query('INSERT INTO user( username ,password) VALUES (?,?)',[username,password], (err,result)=>{
+    con.query(`SELECT username FROM user WHERE username = ?`,[username],(err,result)=>{
         if(err){
             console.log(err)
         }
         else{
-            res.send(result)
+            if(result.length>0){
+                res.send({message:"Username already exist"})
+            }
+            else{
+                con.query('INSERT INTO user( username ,password) VALUES (?,?)',[username,password], (err,result2)=>{
+                    if(err){
+                        console.log(err)
+                    }
+                    else{
+                        res.send(result2)
+                    }
+                })            
+            }
         }
     })
-
 }
 
 module.exports = registerUser;
--- src/AppTopbar.js
+++ src/AppTopbar.js
@@ -1,18 +1,35 @@
-import React, { useContext }  from 'react';
+
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import classNames from 'classnames';
 import { AuthContext } from './context/authContext';
+import { Toast } from 'primereact/toast';
+import { useRef } from 'react';
+import { SplitButton } from "primereact/splitbutton";
+
 
 export const AppTopbar = (props) => {
-const {value} = useContext(AuthContext)
+    const { value,setValue } = useContext(AuthContext);
+    const toast = useRef(null);
+    const items = [
+        {
+            label: 'Logout',
+            icon: 'pi pi-power-off',
+            command: () => { 
+                localStorage.clear();
+                setValue(null)
+            }
+        }
+    ];
     return (
         <div className="layout-topbar">
+                  <Toast ref={toast}></Toast>
             <button type="button" className="p-link  layout-menu-button layout-topbar-button" onClick={props.onToggleMenuClick}>
-                <i className="pi pi-bars"/>
+                <i className="pi pi-bars" />
             </button>
 
             <Link to="/" className="layout-topbar-logo">
-                <img src={props.layoutColorMode === 'light' ? 'assets/layout/images/logo-dark.svg' : 'assets/layout/images/logo-white.svg'} alt="logo"/>
+                <img src={props.layoutColorMode === 'light' ? 'assets/layout/images/logo-dark.svg' : 'assets/layout/images/logo-white.svg'} alt="logo" />
                 <span>Painosoma</span>
             </Link>
 
@@ -20,15 +37,13 @@ const {value} = useContext(AuthContext)
                 <i className="pi pi-ellipsis-v" />
             </button>
 
-                <ul className={classNames("layout-topbar-menu lg:flex origin-top", {'layout-topbar-menu-mobile-active': props.mobileTopbarMenuActive })}>
-                    <li>
-                        <button className="p-link layout-topbar-button" onClick={props.onMobileSubTopbarMenuClick}>
-                            <i className="pi pi-user"/>
-                            <span>Profile</span>
-                        </button>
-        {value}
-                    </li>
-                </ul>
+            <ul className={classNames("layout-topbar-menu lg:flex origin-top", { 'layout-topbar-menu-mobile-active': props.mobileTopbarMenuActive })}>
+                <li>
+                    <SplitButton className='p-button-primary mr-2 mb-2' label={value} icon="pi pi-user"  model={items}  onClick={props.onMobileSubTopbarMenuClick}>
+                      
+                    </SplitButton>
+                </li>
+            </ul>
         </div>
     );
 }
