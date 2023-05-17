4f89f1dc4c412a9bf7d1ac391764af2375e56ccd
Nehashaikh913 <nehashaikh0500@gmail.com>
2023-05-17 16:57:48 +0530

    date functionality in blog

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -91,8 +91,18 @@ function Blogs() {
 
     async function fetchData() {
         const blogData = new apiService();
-        blogData.getBlog().then((data) => {
-            setBlogs(data);
+        await blogData.getBlog().then((data) => {
+            const blogsdate = data.map((blog)=>{
+                let _dates = {...blog}
+                _dates["blogdate"] =  {date : new Date(blog.blogdate)}
+                return (_dates)
+              })
+        
+              blogsdate.sort(
+                (objA, objB) => {
+                   return Number(objB.blogdate.date) - Number(objA.blogdate.date)}
+                )
+            setBlogs(blogsdate);
             setLoading2(false);
         });
     }
@@ -240,9 +250,9 @@ function Blogs() {
         formData.append("reference", data.reference);
 
         if (blog.id) {
-            await Axios.put(`http://192.168.0.143:5000/api/blog/${data.id}`, formData);
+            await Axios.put(`http://localhost:5000/api/blog/${data.id}`, formData);
         } else {
-            await Axios.post("http://192.168.0.143:5000/api/blog", formData);
+            await Axios.post("http://localhost:5000/api/blog", formData);
         }
         setImages([]);
         setImages2([]);
@@ -278,9 +288,10 @@ function Blogs() {
 
     const deleteBlogFunction = async (data) => {
         let selectedIds = typeof data === "number" ? data : data.map((res) => res.id);
-        await Axios.delete(`http://192.168.0.143:5000/api/blog/${selectedIds}`).then();
+        await Axios.delete(`http://localhost:5000/api/blog/${selectedIds}`).then();
         fetchData();
     };
+
 
     const deleteProduct = () => {
         deleteBlogFunction(blog.id);
@@ -372,7 +383,7 @@ function Blogs() {
 
     
     const dateBodyTemplate = (rowData) => {
-        let productDate = new Date(rowData.blogdate);
+        let productDate = new Date(rowData.blogdate.date);
         const result = format(productDate, 'dd/MM/yyyy')
         return (
             <>{result}</>
@@ -607,7 +618,7 @@ function Blogs() {
                                             let image = new FormData();
                                             image.append("image", blobInfo.blob());
                                             try {
-                                                const { data } = await Axios.post("http://192.168.0.143:5000/api/image", image);
+                                                const { data } = await Axios.post("http://localhost:5000/api/image", image);
                                                 success(`assets/demo/images/gallery/${data}`);
                                             } catch (error) {
                                                 console.log(error);
@@ -716,7 +727,7 @@ function Blogs() {
                                     <AccordionTab header="Image Section">
                                         <TabView>
                                             <TabPanel header="upload">
-                                                <FileUpload auto url="http://192.168.0.143:5000/api/image" className="mb-5" onUpload={onImageUpload} name="image[]" accept="image/*" maxFileSize={1000000} />
+                                                <FileUpload auto url="http://localhost:5000/api/image" className="mb-5" onUpload={onImageUpload} name="image[]" accept="image/*" maxFileSize={1000000} />
                                             </TabPanel>
                                             <TabPanel header="Gallery">
                                                 <Button label="select image" icon="pi pi-check" iconPos="right" onClick={(e) => openImageGallery2(e)} />
