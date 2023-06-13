78b50eb075cbb7f06907022c1047ace8fe5a6239
Nehashaikh913 <nehashaikh0500@gmail.com>
2023-06-13 17:58:29 +0530

    context api and blog image

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -97,7 +97,7 @@ function Blogs() {
     }, [dataApi]);
 
     async function fetchData() {
-            const blogsdate = dataApi?.map((blog)=>{
+            const blogsdate = dataApi?.blogs.map((blog)=>{
                 let _dates = {...blog}
                 _dates["blogdate"] =   new Date(blog.blogdate)
                 return (_dates)
@@ -237,9 +237,9 @@ function Blogs() {
         formData.append("reference", data.reference);
 
         if (blog.id) {
-            await Axios.put(`http://192.168.0.143:5000/api/blog/${data.id}`, formData);
+            await Axios.put(`http://localhost:5000/api/blog/${data.id}`, formData);
         } else {
-            await Axios.post("http://192.168.0.143:5000/api/blog", formData);
+            await Axios.post("http://localhost:5000/api/blog", formData);
         }
         setImages([]);
         setImages2([]);
@@ -272,7 +272,7 @@ function Blogs() {
         _blog["feature_image"] = imageArray;
         _blog["blogdate"] = new Date(blog.blogdate);
         setBlog(_blog);
-        setImages2([])
+        setImages2(_blog["feature_image"])
         setProductDialog(true);
     };
 
@@ -283,7 +283,7 @@ function Blogs() {
 
     const deleteBlogFunction = async (data) => {
         let selectedIds = typeof data === "number" ? data : data.map((res) => res.id);
-        await Axios.delete(`http://192.168.0.143:5000/api/blog/${selectedIds}`).then();
+        await Axios.delete(`http://localhost:5000/api/blog/${selectedIds}`).then();
         fetchData();
     };
 
@@ -612,7 +612,7 @@ function Blogs() {
                                             let image = new FormData();
                                             image.append("image", blobInfo.blob());
                                             try {
-                                                const { data } = await Axios.post("http://192.168.0.143:5000/api/image", image);
+                                                const { data } = await Axios.post("http://localhost:5000/api/image", image);
                                                 success(`assets/demo/images/gallery/${data}`);
                                             } catch (error) {
                                                 console.log(error);
@@ -721,7 +721,7 @@ function Blogs() {
                                     <AccordionTab header="Image Section">
                                         <TabView>
                                             <TabPanel header="upload">
-                                                <FileUpload auto url="http://192.168.0.143:5000/api/image" className="mb-5" onUpload={onImageUpload} name="image[]" accept="image/*" maxFileSize={1000000} />
+                                                <FileUpload auto url="http://localhost:5000/api/image" className="mb-5" onUpload={onImageUpload} name="image[]" accept="image/*" maxFileSize={1000000} />
                                             </TabPanel>
                                             <TabPanel header="Gallery">
                                                 <Button label="select image" icon="pi pi-check" iconPos="right" onClick={(e) => openImageGallery2(e)} />
